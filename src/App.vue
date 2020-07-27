50166fbebcdabbb9dd34b9298c2705d8abecefc6
xiezhangrui <15986617642@163.com>
2020-07-27 17:31:11 +0800

    // 在路由上加#...之类的会触发，这里路由定义是history模式，无需hash，回到之前的路由

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ export default {
     window.addEventListener('hashchange', () => {
       var currentPath = window.location.hash.slice(1); // 获取输入的路由
       if (this.$router.path !== currentPath) {
+        // 在路由上加#...之类的会触发，这里路由定义是history模式，无需hash，回到之前的路由
         this.$router.push(currentPath); // 动态跳转
       }
     }, false);
