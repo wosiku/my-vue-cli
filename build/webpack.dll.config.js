5dc9a0e29b203000f96dac8167e537aa6fa00e6e
xiezhangrui <15986617642@163.com>
2020-07-31 17:31:21 +0800

    vendor.dll.js包更小

--- build/webpack.dll.config.js
+++ build/webpack.dll.config.js
@@ -17,6 +17,19 @@ module.exports = {
       path: path.resolve(__dirname, '[name]-manifest.json'),
       name: '[name]_library', 
       context: __dirname
+    }),
+    // 压缩 只是为了包更小一点
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: true,
+        drop_debugger: true
+      },
+      output: {
+        // 去掉注释内容
+        comments: false
+      },
+      sourceMap: true
     })
   ]
 };
